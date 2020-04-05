d0007201715c32c55fa296a38c0d89b1d27dc28c
sabrinasturtevant <sabrina.sturteva@smail.astate.edu>
2020-04-04 21:43:35 -0500

    done

---
+++ scripts.js
@@ -0,0 +1,54 @@
+//Tutorial3
+const button = document.querySelector('button');
+
+button.onclick = function() {
+  let name = prompt('What is your name?');
+  alert('Hello ' + name + 'nice to see you');
+}
+
+
+
+
+// //Tutorial2
+// const btn = document.querySelector('button');
+//
+// const txt = document.querySelector('p');
+//
+// btn.addEventListener('click', updateBtn);
+//
+// function updateBtn() {
+//   if (btn.textContent === "Start Machine") {
+//     btn.textContent = "Stop Machine";
+//     txt.textContent = "The machine has started";
+//   } else {
+//     btn.textContent = "Start Machine";
+//     txt.textContent = "The machine has stopped";
+//   }
+// }
+
+
+
+
+
+////Tutorial1
+// let myName;
+//
+// let myAge = 22;
+//
+// myName = "Sab";
+// myName = "Bob";
+// myAge = 40.25;
+//
+// let test = 6<3;
+//
+// let myNameArray = ["Chris", "Bob", "Jim"];
+// let myNumberArray = [10, 15, 40];
+//
+// let dog = { name: "spot", breed: "dalmation" };
+//
+// let myNumber = 500;
+//
+// const daysInWeek = 7;
+// daysInWeek = 8;
+//
+// alert(daysInWeek);
